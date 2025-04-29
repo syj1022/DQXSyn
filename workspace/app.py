ceaed952e81cbdcda0eb9bf712031163d1bea717
syj1022 <86609584+syj1022@users.noreply.github.com>
2025-04-29 00:47:00 -0400

    Update app.py

--- workspace/app.py
+++ workspace/app.py
@@ -37,13 +37,13 @@ def get_O_G_corr(T):
     thermo_h2 = IdealGasThermo(
         vib_energies=[0.0049848, 0.012958, 0.0161448, 0.5484644],
         potentialenergy=-32.9423,
-        atoms=read('H2/opt.traj'),
+        atoms=read('workspace/H2/opt.traj'),
         geometry='linear', symmetrynumber=2, spin=0)
 
     thermo_h2o = IdealGasThermo(
         vib_energies=[0.02377, 0.02554, 0.20178, 0.46406, 0.47837],
         potentialenergy=-496.2706,
-        atoms=read('H2O/opt.traj'),
+        atoms=read('workspace/H2O/opt.traj'),
         geometry='nonlinear', symmetrynumber=2, spin=0)
 
     return thermo_h2o.get_gibbs_energy(T, 101325, verbose=False) - thermo_h2.get_gibbs_energy(T, 101325, verbose=False) + 2.46 + 460.8683
@@ -63,13 +63,13 @@ def load_sorted_data(T, molar_ratios):
     }
 
     G_corr = {
-        'Ca': get_G_corr('ref/cao_mapping.txt', T),
-        'Mg': get_G_corr('ref/mgo_mapping.txt', T),
-        'Si': get_G_corr('ref/sio2_mapping.txt', T),
+        'Ca': get_G_corr('workspace/ref/cao_mapping.txt', T),
+        'Mg': get_G_corr('workspace/ref/mgo_mapping.txt', T),
+        'Si': get_G_corr('workspace/ref/sio2_mapping.txt', T),
         'O': get_O_G_corr(T),
     }
 
-    with open('ensemble_results.txt', 'r') as f:
+    with open('workspace/ensemble_results.txt', 'r') as f:
         content = f.read()
 
     blocks = content.strip().split('\n\n')
@@ -105,7 +105,7 @@ def load_sorted_data(T, molar_ratios):
             continue
 
         index = os.path.basename(dir_path)
-        G_correction = get_G_corr(f'stable/{index}/mapping.txt', T)
+        G_correction = get_G_corr(f'workspace/stable/{index}/mapping.txt', T)
 
         correction = sum((chemical_potentials.get(elem, 0.0)+G_corr.get(elem, 0.0)) * count for elem, count in composition.items())
         n_atoms = sum(composition.values())
@@ -136,16 +136,16 @@ def load_sorted_data(T, molar_ratios):
 
 # ========== STREAMLIT UI ==========
 
-st.title("🔬 Gibbs Energy-Based Structure Ranking")
+st.title("🔬 Mg-Ca-Si-O")
 
-T = st.slider("Temperature (K)", 300, 2000, 1000, step=50)
+T = st.slider("Temperature (K)", 300, 2000, 1000, step=10)
 col1, col2 = st.columns(2)
 with col1:
-    CaO = st.slider("CaO molar ratio", 0.01, 10.0, 1.0)
-    MgO = st.slider("MgO molar ratio", 0.01, 10.0, 1.0)
+    CaO = st.slider("CaO molar ratio", 0.01, 10.0, 0.1)
+    MgO = st.slider("MgO molar ratio", 0.01, 10.0, 0.1)
 with col2:
-    SiO2 = st.slider("SiO₂ molar ratio", 0.01, 10.0, 1.0)
-    O2 = st.slider("O₂ molar ratio", 0.01, 10.0, 1.0)
+    SiO2 = st.slider("SiO₂ molar ratio", 0.01, 10.0, 0.1)
+    O2 = st.slider("O₂ molar ratio", 0.01, 50.0, 0.1)
 
 sorted_data = load_sorted_data(T, {
     'CaO': CaO, 'MgO': MgO, 'SiO2': SiO2, 'O2': O2
